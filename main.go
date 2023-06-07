736b2e6f444409f29d504d84cb66c077f7bf60a7
cesar <cesarmarcialq@gmail.com>
2023-06-07 16:26:00 -0700

    starting server

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/mar-cial/items/api"
+)
+
+func main() {
+	srv, err := api.CreateServer()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Serving on port %s\n", os.Getenv("SERVERPORT"))
+	log.Fatalln(srv.ListenAndServe())
 }
